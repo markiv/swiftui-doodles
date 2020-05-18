658b6365b93cb9f03740cf9528f3d0c2b9f606ed
Vikram Kriplaney <vikram.kriplaney@netcetera.com>
2020-05-18 09:49:53 +0200

    Initial Commit

---
+++ Doodles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Doodles
+//
+//  Created by Vikram Kriplaney on 18.05.20.
+//  Copyright © 2020 iPhonso GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoodlesTests/DoodlesTests.swift
@@ -0,0 +1,34 @@
+//
+//  DoodlesTests.swift
+//  DoodlesTests
+//
+//  Created by Vikram Kriplaney on 18.05.20.
+//  Copyright © 2020 iPhonso GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import Doodles
+
+class DoodlesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
